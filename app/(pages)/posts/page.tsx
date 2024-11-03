1e6292aa5148c423dac24ad2ff234972145270e6
yuran-nhassengo <yurannhassengo@gmail.com>
2024-11-03 12:07:36 +0200

    add postForm

--- app/(pages)/posts/page.tsx
+++ app/(pages)/posts/page.tsx
@@ -7,19 +7,18 @@ import Link from "next/link";
 export default async function PostsPage() {
 
     const posts = await prisma.post.findMany({
-        where:{
-            title: {
-                contains:"i",
-            },
-        },
+       
         orderBy: {
-            id:"desc",
-        }
+            updatedAt:"desc",
+        },
+        take:2,
     });
+
+    const postsCount = await prisma.post.count();
 
     return (
         <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-            <h1 className="text-3xl font-semibold">All Posts (0)</h1>
+            <h1 className="text-3xl font-semibold">All Posts ({postsCount})</h1>
 
             <ul className="border-t border-b border-black/10 py-5 leading-8">
                 {
@@ -32,6 +31,28 @@ export default async function PostsPage() {
                     ))
                 }
             </ul>
+
+            <form className="flex flex-col  gap-y-2 w-[300px]">
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    className="px-2 py-1 rounded-sm border-2 "
+                />
+                <textarea
+                    name="content"
+                    rows={5}
+                    placeholder="Content"
+                    className="px-2 py-1 mt-2 rounded-sm border-2"
+                />
+                <button
+                    type="submit"
+                    className="bg-blue-500 py-2 text-white rounded-sm"
+                >
+                    Create post
+                </button>
+
+            </form>
 
         </main>
     )
